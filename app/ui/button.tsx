f8783fc5fd42b5d9c6d213e27c072fa58b7a66a3
Emmanuel Yankey <hiyankey@gmail.com>
2025-01-17 14:10:33 +0000

    rm module

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,6 +1,5 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import Link from "next/link";
-import { ClientButton } from "./client-button";
 
 interface ButtonProps extends VariantProps<typeof buttonClasses> {
 	className?: string;
